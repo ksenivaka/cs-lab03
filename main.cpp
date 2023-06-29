d086f3ec525043b3a98a302956e3fed292f2abfb
Kseniya <aaaaa@aaaa>
2023-06-30 00:06:59 +0300

    code: histogram counting added

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
-const auto
+const vector<double>
 input_numbers(size_t count) {
 vector<double> result(count);
 for (size_t i = 0; i < count; i++) {
@@ -15,7 +15,7 @@ return result;
 }
 
 void
-find_minmax(vector<double> numbers, double &min, double &max) {
+find_minmax(const vector<double>& numbers, double &min, double &max) {
 min = numbers[0];
 max = min;
 for (int i = 1; i < numbers.size(); i++) {
@@ -24,24 +24,13 @@ for (int i = 1; i < numbers.size(); i++) {
 }
 }
 
-int main()
-{
-    size_t number_count;
-cout << "Enter number count: ";
-cin >> number_count;
-
-vector<double> numbers = input_numbers(number_count);
-
-    double min, max;
-    find_minmax(numbers, min, max);
-
-    size_t bin_count;
-    cerr << "Enter bin count: ";
-    cin >> bin_count;
-    vector<size_t> bins(bin_count);
-    double bin_size = (max - min) / bin_count;
+vector <size_t> make_histogram(vector<double>numbers,int bin_count) {
+vector<size_t> bins(bin_count);
+double min, max;
+find_minmax(numbers, min, max);
+double bin_size = (max - min) / bin_count;
     size_t max_count = 0;
-    for (size_t i = 0; i < number_count; i++) {
+    for (size_t i = 0; i < numbers.size(); i++) {
         bool found = false;
         for (size_t j = 0; (j < bin_count - 1) && !found; j++) {
             auto lo = min + j * bin_size;
@@ -57,12 +46,29 @@ vector<double> numbers = input_numbers(number_count);
             if (bins[bin_count-1] > max_count) max_count = bins[bin_count-1];
         }
     }
+    return bins;
+}
+
+int main()
+{
+    size_t number_count;
+cout << "Enter number count: ";
+cin >> number_count;
+
+const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cerr << "Enter bin count: ";
+    cin >> bin_count;
+
+    const auto bins = make_histogram(numbers, bin_count);
+//    show_histogram_text(bins);
+
     for (double count : bins) {
-        size_t height = MAX_ASTERISK * (static_cast<double>(count) / max_count);
-        if (height < 100) cout << " ";
-        if (height < 10) cout << " ";
-        cout << height << " | ";
-        for (int i = 0; i < height; i++) cout << "*";
+        //if (height < 100) cout << " ";
+        //if (height < 10) cout << " ";
+        cout << count << " | ";
+        for (int i = 0; i < bins[i]; i++) cout << "*";
         cout << "\n";
     }
 }
